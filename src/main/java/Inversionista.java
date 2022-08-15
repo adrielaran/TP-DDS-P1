ac53c2237495c46e3fc08f2908e7e8d2c173f5f5
unknown <tbarbieribedini@frba.utn.edu.ar>
2022-08-15 20:13:27 -0300

    Avanzo con bases de datos

--- src/main/java/Inversionista.java
+++ src/main/java/Inversionista.java
@@ -1,17 +1,34 @@
+import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
-
+@Entity (name = "Inversionista")
 public class Inversionista {
-    private String nombre;
-    private String apellido;
-    private String correo;
-    private String celular;
-    private List<Criptomoneda> criptomonedas = new ArrayList<>();
-    private Variacion interes;
-<<<<<<< HEAD
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_Inversoinista")
+    private Integer id_Inversionista;
 
-    public void setInteres(Variacion interes) {
-        this.interes = interes;
+    @Column(name = "Nombre")
+    private String nombre;
+
+    @Column(name = "Apellido")
+    private String apellido;
+
+    @Column(name = "Correo")
+    private String correo;
+
+    @Column(name = "Celular")
+    private String celular;
+
+    @OneToMany(mappedBy = "Inversionista", cascade = {CascadeType.ALL}, fetch = FetchType.LAZY)
+    private List<Criptomoneda> criptomonedas;
+
+    @Column(name = "Variacion")
+    private Variacion variacion;
+
+
+    public void setVariacion(Variacion variacion) {
+        this.variacion = variacion;
     }
 
     public void comprobarDisponibilidad() {
@@ -19,9 +36,6 @@ public class Inversionista {
 
     public void analizarTendencias(Integer margen) {
     }
-=======
-    public void setInteres(Variacion interes){ this.interes = interes; }
-    public void comprobarDisponibilidad(){}
-    public void analizarTendencias(Integer margen){}
->>>>>>> 73d170ca0e66a876a864117cdd3b55f8f5d3bcc1
+
+
 }
